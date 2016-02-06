statrting moving average fix

scripts/qnetwork_agent.py
@@ -63,7 +63,9 @@ class QNetworkAgent(agent.Agent):
 
         y = self.network.predict(states, batch_size=self.batch_size)
         y[np.arange(self.batch_size), actions] = targets
-        self.network.fit(states, y, batch_size=self.batch_size, nb_epoch=1, verbose=1)
+        history = self.network.fit(states, y, batch_size=self.batch_size, nb_epoch=1, verbose=0)
+
+        self.logger.log_loss(history.totals['loss'])
 
     def get_action(self, state):
         if random.random() < self.exploration_prob:
@@ -115,7 +117,7 @@ class QNetworkAgent(agent.Agent):
     def convert_state_to_batch(self, state):
         return state.reshape(1,-1)
 
-def build_fully_connected_network(input_shape, output_units, hidden_layer_size=10,
+def build_fully_connected_network(input_shape, output_units, hidden_layer_size=5,
             learning_rate=1e-3, target=False, weight_init='he_normal', 
             activation='relu'):
     trainable = True

tests/test_qnetwork_agent.py
@@ -35,18 +35,18 @@ class TestQNetwork(unittest.TestCase):
         self.assertEquals(actual, expected)
 
     def test_qnetwork_training(self):
-        mdp = mdps.MazeMDP(5, 1)
+        mdp = mdps.MazeMDP(5, 5)
         mdp.compute_states()
         input_shape = np.shape(mdp.get_start_state())
         mdp.EXIT_REWARD = 1
         mdp.MOVE_REWARD = -0.1
         num_actions = len(mdp.get_actions(None))
         discount = mdp.get_discount()
-        a = qnetwork_agent.QNetworkAgent(num_actions=num_actions, input_shape=input_shape, discount=discount, exploration_prob=.5, step_size=1e-3, frozen_update_period=1000, replay_memory_capacity=10000, batch_size=100, exploration_prob_reduction_steps=100000, min_exploration_prob=0.05, logging=True)
-        num_epochs = 10
-        epoch_length = 1000
+        a = qnetwork_agent.QNetworkAgent(num_actions=num_actions, input_shape=input_shape, discount=discount, exploration_prob=.5, step_size=1e-3, frozen_update_period=10000, replay_memory_capacity=10000, batch_size=200, exploration_prob_reduction_steps=1000000, min_exploration_prob=0.05, logging=True)
+        num_epochs = 1
+        epoch_length = 20
         test_epoch_length = 0
-        max_steps = 100
+        max_steps = 1000
         run_tests = False
         e = experiment.Experiment(mdp, a, num_epochs, epoch_length, test_epoch_length, max_steps, run_tests)
         e.run()