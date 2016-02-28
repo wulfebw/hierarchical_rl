8078494a324ed7102ca2158044ede532e9a4c21c
Blake Wulfe <blake.w.wulfe@gmail.com>
2016-02-28 01:21:15 -0800

    full state state adapter

--- scripts/state_adapters.py
+++ scripts/state_adapters.py
@@ -27,3 +27,31 @@ class CoordinatesToSingleRoomRowColAdapter(object):
         formatted_state = np.hstack((row, col))
 
         return formatted_state
+
+class CoordinateTosRowColAdapter(object):
+
+    def __init__(self, room_size, num_rooms):
+        self.room_size = room_size
+        self.num_rooms = num_rooms
+
+    def convert_state_to_agent_format(self, state):
+        """
+        Convert states in format (x, y) to a single room, row, column one-hot vector
+        example: 
+        >>>state = (4, 4)
+        >>>adapter = CoordinatesToSingleRoomRowColAdapter(room_size=3, num_rooms=2)
+        >>>adapter.convert_state_to_agent_format(state)
+        [0,0,0,0,1,0,0,0,0,0,1,0]
+        """
+        ridx, cidx = state
+
+        # find where the agent is in the room
+        row = np.zeros(self.room_size * self.num_rooms)
+        row[ridx] = 1
+        col = np.zeros(self.room_size * self.num_rooms)
+        col[cidx] = 1
+
+        # concat the two vectors
+        formatted_state = np.hstack((row, col))
+
+        return formatted_state
