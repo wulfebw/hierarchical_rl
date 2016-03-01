0a450c3a833968850fbe295398a5edf0d1e6773a
Blake Wulfe <blake.w.wulfe@gmail.com>
2016-03-01 02:00:29 -0800

    row col room state adapter

--- scripts/state_adapters.py
+++ scripts/state_adapters.py
@@ -55,3 +55,32 @@ class CoordinatesToRowColAdapter(object):
         formatted_state = np.hstack((row, col))
 
         return formatted_state
+
+class CoordinatesToRowColRoomAdapter(object):
+
+    def __init__(self, room_size, num_rooms):
+        self.room_size = room_size
+        self.num_rooms = num_rooms
+
+    def convert_state_to_agent_format(self, state):
+        """
+        Convert states in format (x, y) to a single room, row, column one-hot vector
+        _with_ an additional one-hot vector identifying the room
+        example: 
+        >>>state = (4, 4)
+        >>>adapter = CoordinatesToSingleRoomRowColAdapter(room_size=3, num_rooms=2)
+        >>>adapter.convert_state_to_agent_format(state)
+        [1,0,0,1,0,0,0,0,0,1]
+        """
+        ridx, cidx = state
+
+        # find where the agent is in the room
+        row = np.zeros(self.room_size)
+        row[ridx % self.room_size] = 1
+        col = np.zeros(self.room_size)
+        col[cidx % self.room_size] = 1
+        room = np.zeros(self.num_rooms ** 2)
+        # concat the three vectors
+        formatted_state = np.hstack((row, col))
+
+        return formatted_state
--- tests/test_state_adapters.py
+++ tests/test_state_adapters.py
@@ -67,6 +67,38 @@ class TestCoordinatesToRowColAdapter(unittest.TestCase):
         actual = adapter.convert_state_to_agent_format(mdp_formatted_state).tolist()
         self.assertEquals(actual, expected)
 
+class TestCoordinatesToRowColRoomAdapter(unittest.TestCase):
+
+    def test_convert_state_to_agent_format_first_room(self):
+        adapter = state_adapters.CoordinatesToSingleRoomRowColAdapter(room_size=3)
+        mdp_formatted_state = (2, 2)
+        expected = [0,0,1,0,0,1]
+        actual = adapter.convert_state_to_agent_format(mdp_formatted_state).tolist()
+        self.assertEquals(actual, expected)
+
+    def test_convert_state_to_agent_format_fourth_room(self):
+        adapter = state_adapters.CoordinatesToSingleRoomRowColAdapter(room_size=3)
+        mdp_formatted_state = (4, 4)
+        expected = [0,1,0,0,1,0]
+        actual = adapter.convert_state_to_agent_format(mdp_formatted_state).tolist()
+        self.assertEquals(actual, expected)
+
+    def test_convert_state_to_agent_format_off_diagonal_room(self):
+        adapter = state_adapters.CoordinatesToSingleRoomRowColAdapter(room_size=3)
+        mdp_formatted_state = (0, 4)
+        expected = [1,0,0,0,1,0]
+        actual = adapter.convert_state_to_agent_format(mdp_formatted_state).tolist()
+        self.assertEquals(actual, expected)
+
+    def test_convert_state_to_agent_format_off_fourth_room_first_square(self):
+        adapter = state_adapters.CoordinatesToSingleRoomRowColAdapter(room_size=3)
+        mdp_formatted_state = (3, 3)
+        expected = [1,0,0,1,0,0]
+        actual = adapter.convert_state_to_agent_format(mdp_formatted_state).tolist()
+        self.assertEquals(actual, expected)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
